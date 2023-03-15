559b2a4779a990b08895edec6c1362ee8430aea4
Ch. Sai Kumar <96567025+95027@users.noreply.github.com>
2023-03-15 21:17:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const inputEl=document.querySelector('input');
+
+const bodyEl=document.querySelector('body');
+
+inputEl.checked=JSON.parse(localStorage.getItem("mode"));
+
+updateBody();
+
+function updateBody()
+{
+    if(inputEl.checked)
+    {
+        bodyEl.style.backgroundColor="#000";
+    }
+    else
+    {
+        bodyEl.style.backgroundColor="#fff";
+    }
+}
+
+inputEl.addEventListener('input',()=>{
+    updateBody();
+    updateMode();
+})
+
+function updateMode()
+{
+    localStorage.setItem('mode',JSON.stringify(inputEl.checked));
+}
